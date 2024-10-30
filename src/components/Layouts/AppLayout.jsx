71da0ad9a99fd9a96e5de77e6cc78006b3937a70
Nikhil Jayant <104789911+nikhiljayant@users.noreply.github.com>
2024-10-30 23:53:05 +0530

    supabase setup

--- src/components/Layouts/AppLayout.jsx
+++ src/components/Layouts/AppLayout.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 // Dependency
 import { Outlet } from "react-router-dom";
+// Components
+import Header from "../Common/Header";
 
 const AppLayout = () => {
   return (
     <div>
       <div className="grid-background"></div>
-      <Outlet />
+      <main className="min-h-screen px-20">
+        <Header />
+        <Outlet />
+      </main>
+      <div className="p-10 text-center bg-gray-800 mt-10">
+        Made with ❤️ by Nikhil Jayant
+      </div>
     </div>
   );
 };
