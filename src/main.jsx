008105d36fcd32ff079e3d58f875281324fb5709
Nikhil Jayant <104789911+nikhiljayant@users.noreply.github.com>
2024-10-31 12:11:43 +0530

    clerk setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+// Components
+import App from "./App.jsx";
+// CSS
+import "./index.css";
+// Dependency
+import { ClerkProvider } from "@clerk/clerk-react";
+import { shadesOfPurple } from "@clerk/themes";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Missing Publishable Key");
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ClerkProvider
+      appearance={{ baseTheme: shadesOfPurple }}
+      publishableKey={PUBLISHABLE_KEY}
+      afterSignOutUrl="/"
+    >
+      <App />
+    </ClerkProvider>
+  </React.StrictMode>
+);
