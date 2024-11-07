b03ae44dd82b75b9d042aa3f5dc4a728c73c6258
Nikhil Jayant <104789911+nikhiljayant@users.noreply.github.com>
2024-11-07 23:39:15 +0530

    fetch data custom hook

--- src/components/Pages/JobListing.jsx
+++ src/components/Pages/JobListing.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect } from "react";
+// Api
+import { getJobs } from "@/api/apiJobs";
+// Custom Hook
+import useFetch from "@/hooks/useFetch";
 
 const JobListing = () => {
-  return (
-    <div>JobListing</div>
-  )
-}
+  const {
+    fn: fnJobs,
+    data: dataJobs,
+    loading: loadingJobs,
+  } = useFetch(getJobs);
 
-export default JobListing
+  useEffect(() => {
+    fnJobs();
+  }, []);
+
+  return <div>JobListing</div>;
+};
+
+export default JobListing;
