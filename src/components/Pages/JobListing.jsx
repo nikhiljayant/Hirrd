099cbe0207ad3cb027f7962350d0c6a4cf1ddb38
Nikhil Jayant <104789911+nikhiljayant@users.noreply.github.com>
2024-11-11 23:43:23 +0530

    search and filter jobs

--- src/components/Pages/JobListing.jsx
+++ src/components/Pages/JobListing.jsx
@@ -8,11 +8,24 @@ import { useUser } from "@clerk/clerk-react";
 import { BarLoader } from "react-spinners";
 // Components
 import JobCard from "../Partials/JobCard";
+import { getCompanies } from "@/api/apiCompanies";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+import { State } from "country-state-city";
 
 const JobListing = () => {
   const [searchQuery, setSearchQuery] = useState("");
   const [location, setLocation] = useState("");
-  const [companyId, setCompanyId] = useState("");
+  const [company_id, setCompanyId] = useState("");
 
   const { isLoaded } = useUser();
 
@@ -20,23 +33,108 @@ const JobListing = () => {
     fn: fnJobs,
     data: jobs,
     loading: loadingJobs,
-  } = useFetch(getJobs, { location, companyId, searchQuery });
+  } = useFetch(getJobs, { location, company_id, searchQuery });
+
+  const { fn: fnCompanies, data: companies } = useFetch(getCompanies);
+
+  useEffect(() => {
+    if (isLoaded) {
+      fnCompanies();
+    }
+  }, [isLoaded]);
 
   useEffect(() => {
     if (isLoaded) {
       fnJobs();
     }
-  }, [isLoaded, location, companyId, searchQuery]);
+  }, [isLoaded, location, company_id, searchQuery]);
 
   if (!isLoaded) {
     return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
   }
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+
+    let formData = new FormData(e.target);
+
+    const query = formData.get("search-query");
+    if (query) setSearchQuery(query);
+  };
+
+  const clearFilters = () => {
+    setSearchQuery("");
+    setLocation("");
+    setCompanyId("");
+  };
 
   return (
     <div>
       <h1 className="gradient-title font-extrabold text-6xl sm:text-7xl text-center pb-8">
         Latest jobs
       </h1>
+
+      <form
+        onSubmit={handleSearch}
+        className="h-14 flex w-full gap-2 items-center mb-3"
+      >
+        <Input
+          type="text"
+          placeholder="Search Jobs by Title..."
+          name="search-query"
+          className="h-full flex-1 px-4 text-md"
+        />
+        <Button type="submit" className="h-full sm:w-28" variant="blue">
+          Search
+        </Button>
+      </form>
+
+      <div className="flex flex-col sm:flex-row gap-2">
+        <Select value={location} onValueChange={(value) => setLocation(value)}>
+          <SelectTrigger>
+            <SelectValue placeholder="Filter by Location" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              {State.getStatesOfCountry("IN").map(({ name }) => {
+                return (
+                  <SelectItem key={name} value={name}>
+                    {name}
+                  </SelectItem>
+                );
+              })}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+
+        <Select
+          value={company_id}
+          onValueChange={(value) => setCompanyId(value)}
+        >
+          <SelectTrigger>
+            <SelectValue placeholder="Filter by Company" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              {companies?.map(({ name, id }) => {
+                return (
+                  <SelectItem key={name} value={id}>
+                    {name}
+                  </SelectItem>
+                );
+              })}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+
+        <Button
+          onClick={clearFilters}
+          variant="destructive"
+          className="sm:w-1/2"
+        >
+          Clear Filters
+        </Button>
+      </div>
 
       {loadingJobs && (
         <BarLoader className="mt-4" width={"100%"} color="#36d7b7" />
--- src/components/Partials/JobCard.jsx
+++ src/components/Partials/JobCard.jsx
@@ -46,7 +46,7 @@ const JobCard = ({
   }, [savedJob]);
 
   return (
-    <Card>
+    <Card className="flex flex-col">
       <CardHeader>
         <CardTitle className="flex justify-between font-bold">
           {job.title}
