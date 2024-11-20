908f48a007580ea97aefe32a73d96845882f725e
Nikhil Jayant <104789911+nikhiljayant@users.noreply.github.com>
2024-11-20 23:05:49 +0530

    create applications page

---
+++ src/components/Partials/CreatedApplications.jsx
@@ -0,0 +1,38 @@
+import { getApplications } from "@/api/apiApplications";
+import useFetch from "@/hooks/useFetch";
+import { useUser } from "@clerk/clerk-react";
+import React from "react";
+import { BarLoader } from "react-spinners";
+import ApplicationCard from "../Common/ApplicationCard";
+
+const CreatedApplications = () => {
+  const { user } = useUser();
+
+  const {
+    loading: loadingApplications,
+    data: dataApplications,
+    fn: fnApplications,
+  } = useFetch(getApplications);
+
+  useEffect(() => {
+    fnApplications();
+  }, []);
+
+  if (loadingApplications) {
+    return <BarLoader className="mb-4" width={"100%"} color="#36d7b7" />;
+  }
+
+  return (
+    <div className="flex flex-col gap-2">
+      {dataApplications.map((application) => (
+        <ApplicationCard
+          key={application.id}
+          application={application}
+          isCandidate={true}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default CreatedApplications;
